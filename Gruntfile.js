3e90a8acdc96e6e58c1a48a345a733435a758932
luojinghui <939035243@qq.com>
2016-04-19 16:52:37 +0800

    add .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,12 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),     //存储在package.json文件中的JSON元数据引入到grunt config中
-    // banner: '/*!\n' +
-    //         ' * version:<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-    //         ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-    //         ' * Licensed under the <%= pkg.license %> license\n' +
-    //         ' */\n',
+    banner: '/*!\n' +
+            ' * version:<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2011-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under the <%= pkg.license %> license\n' +
+            ' */\n',
     uglify: {                                    //指定了一个banner选项(用于在文件顶部生成一个注释)，
         options: {
             stripBanners: true,                    //紧接着是一个单一的名为build的uglify目标，用于将一个js文件压缩为一个目标文件
@@ -33,33 +32,56 @@ module.exports = function(grunt) {
     watch: {
         files: ['public/*.js'],
         tasks: ['jshint']
+    },
+    concat:{
+      options: {
+        stripBanners: true,
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */',
+        //定义一个字符串插入没个文件之间用于连接输出
+        separator: ';'
+      },
+      dist: {
+          src: ['public/question.js','public/Answer.js'],
+          dest: 'build/concat.js'
+      }
+    },
+    less: {
+      development: {
+        banner: '/*! author: luo \n version: <%= pkg.version %> \n' +
+        'time: <%= grunt.template.today("yyyy-mm-dd") %> */\n' + 'email: luojinghui424@gmail.com \n',
+        options: {
+          paths: ['assets/css']
+        },
+        files: {
+          'path/to/result.css': 'path/to/source.less'
+        }
+      },
+      production: {
+        options: {
+          paths: ['assets/css'],
+          plugins: [
+            new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]}),
+            new (require('less-plugin-clean-css'))(cleanCssOptions)
+          ],
+          modifyVars: {
+            imgPath: '"http://mycdn.com/path/to/images"',
+            bgColor: 'red'
+          }
+        },
+        files: {
+          'path/to/result.css': 'path/to/source.less'
+        }
+      }
     }
-    // watch: {
-    //     css: {
-    //         files: ['public/*.css'],
-    //         tasks: ['compass'],
-    //         options: {
-    //         // Start a live reload server on the default port 35729
-    //         livereload: true,
-    //         },
-    //     },
-    //     another: {
-    //         files: ['lib/*.js'],
-    //         tasks: ['anothertask'],
-    //         options: {
-    //         // Start another live reload server on port 1337
-    //         livereload: 1337,
-    //         },
-    //     }
-    // }
   });
 
   // 加载包含 "uglify" 任务的插件。
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks("grunt-contrib-jshint");
-
-
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-less');
 
 
     // grunt.loadNpmTasks("grunt-contrib-copy");
@@ -73,6 +95,5 @@ module.exports = function(grunt) {
   // 默认被执行的任务列表。
   grunt.registerTask('default', ['uglify','jshint']);
   grunt.registerTask('check', ['jshint']);
-
 
 };
